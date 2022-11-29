cf49db6e19a2035f51c3f32ebb5c5a8021d3a693
Chamara-Wijepala <mr.worf777@gmail.com>
2022-11-29 21:54:06 +0530

    Allow user to switch between light and dark themes

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,19 @@
+import { useState, useEffect } from "react";
+
 export default function Header() {
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark-theme");
+  }, [isDark]);
+
   return (
     <header className="primary-header | bg-primary-100">
-      <div className="primary-header__content | container">
+      <div className="primary-header__content | container clr-secondary">
         <h1 className="fs-700 fw-bold">Where in the world?</h1>
-        <button type="button">Dark Mode</button>
+        <button type="button" onClick={() => setIsDark(!isDark)}>
+          Dark Mode
+        </button>
       </div>
     </header>
   );
