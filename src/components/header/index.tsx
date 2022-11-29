c14c0302cecf25934730434829cca5db2c6fc0e9
Chamara-Wijepala <mr.worf777@gmail.com>
2022-11-29 21:14:11 +0530

    Add header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="primary-header | bg-primary-100">
+      <div className="primary-header__content | container">
+        <h1 className="fs-700 fw-bold">Where in the world?</h1>
+        <button type="button">Dark Mode</button>
+      </div>
+    </header>
+  );
+}
