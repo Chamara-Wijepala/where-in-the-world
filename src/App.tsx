4fde1babbca05c3b0014e8b3dca7c0786675a967
Chamara-Wijepala <mr.worf777@gmail.com>
2022-12-01 15:23:15 +0530

    Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "components/header";
+import Home from "pages/home";
+import Details from "pages/details";
 
 function App() {
   return (
     <div>
       <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Details />} />
+      </Routes>
     </div>
   );
 }
