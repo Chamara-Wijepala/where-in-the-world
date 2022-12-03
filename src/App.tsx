fbaddaea77c75683f31a1ee6795687790d0966c6
Chamara-Wijepala <mr.worf777@gmail.com>
2022-12-03 15:51:10 +0530

    Move header component into App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import Header from "components/header";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
 import Home from "pages/home";
 import Details from "pages/details";
 
-function App() {
+export default function App() {
   return (
     <div>
       <Header />
@@ -16,4 +17,29 @@ function App() {
   );
 }
 
-export default App;
+function Header() {
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark-theme");
+  }, [isDark]);
+
+  return (
+    <header className="primary-header | bg-primary-100 box-shadow">
+      <div className="primary-header__content | container clr-secondary">
+        <h1 className="fs-700 fw-bold">Where in the world?</h1>
+        <button type="button" onClick={() => setIsDark(!isDark)}>
+          {isDark ? (
+            <span className="primary-header__toggle-theme-btn-content">
+              <BsMoonFill /> Dark Mode
+            </span>
+          ) : (
+            <span className="primary-header__toggle-theme-btn-content">
+              <BsSunFill /> Light Mode
+            </span>
+          )}
+        </button>
+      </div>
+    </header>
+  );
+}
