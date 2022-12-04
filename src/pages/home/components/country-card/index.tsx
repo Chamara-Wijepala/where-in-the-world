62e10584d9a74fa24fea004ee98a7559f4822065
Chamara-Wijepala <mr.worf777@gmail.com>
2022-12-04 22:43:15 +0530

    Change font sizes in country card

--- src/pages/home/components/country-card/index.tsx
+++ src/pages/home/components/country-card/index.tsx
@@ -7,18 +7,18 @@ export default function CountryCard({ data }: { data: ICountry }) {
     <div className="card box-shadow">
       <img src={data.flags.svg} alt={`the flag of ${data.name.common}`} />
       <div className="card__details | clr-secondary">
-        <h2 className="fw-bold">{data.name.common}</h2>
-        <ul role="list" className="fs-350">
+        <h2 className="fs-500 fw-bold">{data.name.common}</h2>
+        <ul role="list" className="fs-400 fw-small">
           <li>
-            <span className="fw-bold">Population: </span>
+            <span className="fw-regular">Population: </span>
             {numberFormat.format(data.population)}
           </li>
           <li>
-            <span className="fw-bold">Region: </span>
+            <span className="fw-regular">Region: </span>
             {data.region}
           </li>
           <li>
-            <span className="fw-bold">Capital: </span>
+            <span className="fw-regular">Capital: </span>
             {data.capital.join(", ")}
           </li>
         </ul>
